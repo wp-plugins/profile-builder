d44037bc3f5d524d9eba65594f594c55372d409d
reflectionmedia <reflectionmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-13 13:16:17 +0000

    git-svn-id: https://plugins.svn.wordpress.org/profile-builder@372484 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/uninstall.php
+++ trunk/uninstall.php
@@ -1,10 +1,2 @@
-<?php
-
-if( !defined( 'WP_UNINSTALL_PLUGIN' ) )			
-	exit ();									// If uninstall not called from WordPress exit
-
-delete_option( 'wppb_default_settings' );	    // Delete default settings from options table
-delete_option( 'wppb_default_style' );			// Delete "use default css or not" settings
-delete_option( 'wppb_display_admin_settings' ); // Delete display admin bar option
-
-?>
+<?phpif( !defined( 'WP_UNINSTALL_PLUGIN' ) )				exit ();									// If uninstall not called from WordPress exit
+delete_option( 'wppb_default_settings' );	    // Delete default settings from options tabledelete_option( 'wppb_default_style' );			// Delete "use default css or not" settingsdelete_option( 'wppb_display_admin_settings' ); // Delete display admin bar option
