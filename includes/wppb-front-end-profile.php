89ff39924fbcc9ed70e2b984e2bda726d035d9b9
reflectionmedia <reflectionmedia@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-15 06:16:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/profile-builder/trunk@384910 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/wppb-front-end-profile.php
+++ includes/wppb-front-end-profile.php
@@ -1,12 +1,46 @@
 <?php
+/*
+	wp_update_user only attempts to clear and reset cookies if it's updating the password.
+	The php function setcookie(), used in both the cookie-clearing and cookie-resetting functions,
+	adds to the page headers and therefore must be called within the first php tag on the page, and 
+	before the WordPress get_header() function. Since wp_update_user needs this, it must be at the 
+	beginning of the page as well.
+*/
+$wppb_saved_pass = false;
+$wppb_error_pass = false;
+
+function wppb_save_the_password(){
+	global $wppb_saved_pass;
+	global $wppb_error_pass;
+	/* Load registration file. */
+	require_once(ABSPATH . WPINC . '/registration.php');
+	/* Get user info. */
+	global $current_user;	
+	if ( 'POST' == $_SERVER['REQUEST_METHOD'] && !empty( $_POST['action'] ) && $_POST['action'] == 'update-user' && wp_verify_nonce($_POST['edit_nonce_field'],'verify_edit_user') ) { 
+		/* Update user password. */
+		if ( !empty($_POST['pass1'] ) && !empty( $_POST['pass2'] ) ) {
+			if ( $_POST['pass1'] == $_POST['pass2'] )
+			{
+				wp_update_user( array( 'ID' => $current_user->id, 'user_pass' => esc_attr( $_POST['pass1'] ) ) );
+				$wppb_saved_pass = true;
+			} else {
+				$wppb_error_pass = true;
+			}
+		}	
+	}
+}
+add_action('init', 'wppb_save_the_password');
+
+/* the shortcode function */
 function wppb_front_end_profile_info() {
+	global $wppb_saved_pass;
+	global $wppb_error_pass;
+	
 	ob_start();
 	get_currentuserinfo();
 	$wppb_defaultOptions = get_option('wppb_default_settings');
 	$changesSaved = 'no';
-	
-	
-	
+
 	/* Load registration file. */
 		require_once(ABSPATH . WPINC . '/registration.php');
 	/* Get user info. */
@@ -19,12 +53,11 @@ function wppb_front_end_profile_info() {
 		else $thisEmail = $current_user->id;
 
 		/* Update user password. */
-		if ( !empty($_POST['pass1'] ) && !empty( $_POST['pass2'] ) ) {
-			if ( $_POST['pass1'] == $_POST['pass2'] ){
-				wp_update_user( array( 'ID' => $current_user->id, 'user_pass' => esc_attr( $_POST['pass1'] ) ) );
-				$changesSaved = 'yes';
-			}else
-				$error = __('The passwords you entered didn\'t match.  Your password was not updated.', 'profilebuilder');
+		if ($wppb_saved_pass == true){
+			$changesSaved = 'yes';
+		}
+		if ($wppb_error_pass == true){
+			$error = __('The passwords you entered didn\'t match.  Your password was not updated.', 'profilebuilder');
 		}
 		
 		
@@ -279,7 +312,6 @@ function wppb_front_end_profile_info() {
 	$output = ob_get_contents();
     ob_end_clean();
     return $output;
-
 }
 
 ?>
