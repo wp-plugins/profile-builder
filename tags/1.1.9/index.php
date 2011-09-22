01207292758ed90d1921f3b60af06d993fa4e036
barinagabriel <barinagabriel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-09-22 08:41:34 +0000

    git-svn-id: https://plugins.svn.wordpress.org/profile-builder@441996 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.1.9/index.php
+++ tags/1.1.9/index.php
@@ -54,7 +54,7 @@ function return_bytes($val) {
 } 
  
  
-define( 'ProfileBuilderVersion', '1.1.7' );
+define( 'ProfileBuilderVersion', '1.1.9' );
 define( 'wppb_plugin_dir', WP_PLUGIN_DIR . '/' . dirname( plugin_basename( __FILE__ ) ) );
 define( 'wppb_plugin_url', WP_PLUGIN_URL . '/' . dirname( plugin_basename( __FILE__ ) ) );
 define( 'ServerMaxUploadSizeByte', return_bytes( ini_get( 'upload_max_filesize') ) );
